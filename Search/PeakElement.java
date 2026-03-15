81098f44b82be68a9abb4e7af5da805f373adb53
GANTA MANIKANTA  ANJANEYA <gantamanikanta46@gmail.com>
2026-03-15 20:51:47 +0530

    Update PeakElement.java

--- Search/PeakElement.java
+++ Search/PeakElement.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        int low=0,high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+}
