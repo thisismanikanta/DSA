3fdaeda1b952a0eb68de108e9fc6f9f77ab6ea6f
GANTA MANIKANTA  ANJANEYA <gantamanikanta46@gmail.com>
2026-07-16 22:27:41 +0530

    Add cycle detection in linked list
    Implement Floyd's cycle-finding algorithm to detect cycles in a linked list.

---
+++ LinkedLists/LinkedListCycle.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
+
+// output
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+// Example 2:
+
+// Input: head = [1,2], pos = 0
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
